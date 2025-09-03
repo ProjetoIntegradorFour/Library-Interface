ef7361cb225ab32e57b9b646e9c8244998809774
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2025-09-03 16:55:53 -0300

    fix: change label and margins

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -7,12 +7,10 @@ export default function Login() {
         <h2>Biblioteca Virtual</h2>
         <form>
           <div className="input-group">
-            <label htmlFor="email">Email</label>
             <input type="email" id="email" placeholder="Digite seu email" required />
           </div>
 
           <div className="input-group">
-            <label htmlFor="password">Senha</label>
             <input type="password" id="password" placeholder="Digite sua senha" required />
           </div>
 
