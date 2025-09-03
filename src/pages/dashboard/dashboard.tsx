72b46f9184689988e48f2a9957ed09294e2cc4df
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2025-09-03 14:07:48 -0300

    feat: login page interface

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,7 +2,7 @@ import "./dashboard.css"
 
 export default function Dashboard() {
   return (
-    <div className="page-content" style={{ padding: "10px", backgroundColor: "lightblue", marginTop: "20px" }}>
+    <div className="page-content" style={{ padding: "10px", backgroundColor: "lightblue", marginTop: "50px" }}>
       <h1>🚀 Testando Dashboard</h1>
     </div>
   );
--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,9 +1,24 @@
-import "./login.css"
+import "./login.css";
 
 export default function Login() {
   return (
-    <div className="page-content" style={{ padding: "10px", backgroundColor: "lightblue", marginTop: "50px" }}>
-      <h1>🚀 Testando Login</h1>
+    <div className="page-content">
+      <div className="login-box">
+        <h2>Biblioteca Virtual</h2>
+        <form>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Digite seu email" required />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Senha</label>
+            <input type="password" id="password" placeholder="Digite sua senha" required />
+          </div>
+
+          <button type="submit" className="login-btn">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
