15b1da980b8047613d85fbefef23f2f3382fbfc0
Jaummzito <jp2939314@gmail.com>
2025-09-03 09:53:53 -0300

    feat: fix colors in sidebar.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import "@fontsource/racing-sans-one/400.css"
+import "@fontsource/roboto/500.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
