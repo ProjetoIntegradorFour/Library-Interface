46fc885149474aeb007d9cc49b9c9963eef6d8b5
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2025-09-08 19:48:02 -0300

    build: folder structure

---
+++ src/pages/acervo.tsx
@@ -0,0 +1,9 @@
+import "./acervo.css"
+
+export default function Acervo() {
+  return (
+    <div className="page-content" style={{ padding: "10px", backgroundColor: "lightblue", marginTop: "50px" }}>
+      <h1>🚀 Testando atualização do Acervo</h1>
+    </div>
+  );
+}
