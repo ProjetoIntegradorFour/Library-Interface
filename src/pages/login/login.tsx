628179daf47c9b9404aa8719142219b485257278
thomasteinhoff <thomas11ventura@gmail.com>
2025-09-08 20:30:23 -0300

    perf(login page optimization): improved css and html code

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -3,20 +3,16 @@ import "./login.css";
 export default function Login() {
   return (
     <div className="login-page">
+
       <div className="login-box">
-        <h2>Biblioteca Virtual</h2>
+        <h1>Biblioteca Virtual</h1>
         <form>
-          <div className="input-group">
-            <input type="email" id="email" placeholder="Digite seu email" required />
-          </div>
-
-          <div className="input-group">
-            <input type="password" id="password" placeholder="Digite sua senha" required />
-          </div>
-
-          <button type="submit" className="login-btn">Entrar</button>
+          <input type="email" id="email" placeholder="Digite seu email" required autoComplete="off" title="Insert your Email" />
+          <input type="password" id="password" placeholder="Digite sua senha" required autoComplete="off" title="Insert your Password" />
+          <button type="submit">Entrar</button>
         </form>
       </div>
+
     </div>
   );
 }
