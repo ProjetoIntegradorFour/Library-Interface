b7cde184535bfd467abae29ceb31d89aa3ce545e
GuilhermeAmargo <guilhermecamargosilva211@gmail.com>
2025-09-15 21:01:27 -0300

    feat: linegraph component

---
+++ src/components/LineGraph/LineChartLoans.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import './LineChartLoans.css';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+interface LoanData {
+  month: string;
+  amount: number;
+}
+
+interface LineChartLoansProps {
+  data: LoanData[];
+}
+
+const LineChartLoans: React.FC<LineChartLoansProps> = ({ data }) => {
+  const chartData = {
+    labels: data.map(d => d.month),
+    datasets: [
+      {
+        label: 'Quantia por mês',
+        data: data.map(d => d.amount),
+        borderColor: 'rgba(75,192,192,1)',
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        tension: 0.4, // suaviza a linha
+        fill: true,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top' as const,
+      },
+      title: {
+        display: true,
+        text: 'Empréstimos por mês',
+      },
+    },
+    scales: {
+      x: {
+        grid: {
+          color: '#e0e0e0', // grade cinza clara
+        },
+      },
+      y: {
+        grid: {
+          color: '#e0e0e0',
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="line-chart-wrapper">
+      <div className="line-chart-container">
+        <Line data={chartData} options={options} />
+      </div>
+    </div>
+  );
+
+};
+
+export default LineChartLoans;
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,4 +1,5 @@
 import "../styles/global.css";
+<<<<<<< Updated upstream
 import Table from "../components/Table/Table";
 
 export default function Dashboard() {
@@ -8,6 +9,26 @@ export default function Dashboard() {
     >
       {/* Tabela */}
       <Table />
+=======
+import LineChartLoans from "../components/LineGraph/LineChartLoans";
+
+
+const LoanData = [
+  { month: 'Janeiro', amount: 5000 },
+  { month: 'Fevereiro', amount: 7000 },
+  { month: 'Março', amount: 6000 },
+  { month: 'Abril', amount: 8000 },
+  { month: 'Maio', amount: 9000 },
+  { month: 'Junho', amount: 7500 },
+  { month: 'Julho', amount: 8500 },
+];
+
+export default function Dashboard() {
+  return (
+    <div className="dashboard-page" style={{ padding: "10px", backgroundColor: "lightblue", marginTop: "50px" }}>
+      <h1>🚀 Testando Dashboard</h1>
+      <LineChartLoans data={LoanData} />
+>>>>>>> Stashed changes
     </div>
   );
 }
